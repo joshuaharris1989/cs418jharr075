c818c2c779ce7dd4cc051fd7778421b92dd5fb65
joshuaharris1989 <joshuaharris1989@gmail.com>
2026-04-06 10:10:33 -0400

    fixed railway db env

--- Project/server/database/connection.js
+++ Project/server/database/connection.js
@@ -1,12 +1,12 @@
-import 'dotenv/config';
-import mysql from 'mysql2/promise';
+import "dotenv/config";
+import mysql from "mysql2/promise";
 
-const connection =await mysql.createConnection({
-    host:process.env.DB_HOST,
-    user:process.env.DB_USER,
-    password:process.env.DB_PASSWORD,
-    database:process.env.DB_NAME
-})
+const connection = await mysql.createConnection({
+  host: process.env.DB_HOST,
+  port: Number(process.env.DB_PORT) || 3306,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
 
 export { connection };
-
